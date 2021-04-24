2e7ed7e5eb9fedc6e8454c95ef3b5a6e3fc7059f
akino <akikoba2001@gmail.com>
2021-04-24 22:50:37 +0900

    #1 simple signup function

---
+++ backend/accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
+from .models import User
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('accounts:signup')
+    else:
+        form = CustomUserCreationForm()
+    
+    return render(request, 'accounts/signup.html', {'form': form})
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'accounts.User'

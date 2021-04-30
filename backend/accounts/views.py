755232a43f330c8e9cb57fd70f059c2836d95713
akino <akikoba2001@gmail.com>
2021-04-30 22:09:28 +0900

    Add profileUpdateView

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import CustomUserCreationForm
 from django.contrib.auth import login
-from .models import Profile, User
-from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.views import generic
+
+from .forms import CustomUserCreationForm
+from .models import Profile
 
 def signup(request):
     if request.user.is_authenticated:
@@ -26,3 +28,12 @@ class ProfileView(LoginRequiredMixin, generic.DetailView):
     model = Profile
     template_name = 'accounts/profile.html'
     context_object_name = 'profile'
+
+class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Profile
+    fields = ['text', 'icon_image', 'header_image']
+    template_name = 'accounts/profile_update.html'
+    context_object_name = 'profile'
+    
+    def get_success_url(self):
+        return reverse('accounts:profile_detail', args=[self.kwargs['pk']])

cb76afc4225ce6e8e46b3f51b41000d87e2cf9d8
akino <akikoba2001@gmail.com>
2021-05-01 18:15:53 +0900

    Make readable

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
-from django.views import generic
+from django.views.generic import TemplateView, DetailView, UpdateView
 
 from .forms import CustomUserCreationForm
 from .models import Profile
@@ -21,19 +21,22 @@ def signup(request):
         form = CustomUserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
-class HomeView(LoginRequiredMixin, generic.TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name="accounts/home.html"
 
-class ProfileView(LoginRequiredMixin, generic.DetailView):
+class ProfileView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'accounts/profile.html'
     context_object_name = 'profile'
 
-class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = Profile
     fields = ['text', 'icon_image', 'header_image']
     template_name = 'accounts/profile_update.html'
     context_object_name = 'profile'
     
     def get_success_url(self):
-        return reverse('accounts:profile_detail', args=[self.kwargs['pk']])
+        return reverse(
+            'accounts:profile_detail', 
+            args=[self.kwargs['pk']]
+            )

c5dbcf26ba1ca60e802d848f2a1ab8975955eae6
Akinori <65198192+Akinori13@users.noreply.github.com>
2021-05-03 15:01:10 +0900

    Revert "Revert "Feature/simple speak function""

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'speaks.apps.SpeaksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('speaks/', include('speaks.urls')),
 ]
 
 if settings.DEBUG:

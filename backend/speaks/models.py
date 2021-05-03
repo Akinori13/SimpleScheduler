925c327ff68a70285e22c5ab0186f883d896f06a
akino <akikoba2001@gmail.com>
2021-05-03 14:50:17 +0900

    create speak model in speaks app

--- backend/speaks/models.py
+++ backend/speaks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Speak(models.Model):
+    content = models.TextField(max_length=140)
+    user = models.ForeignKey(
+        'accounts.User',
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:20]

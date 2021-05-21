ddbe327fd2d268e5ff68da7737d1e673b6c90b5f
akino <akikoba2001@gmail.com>
2021-05-21 23:39:58 +0900

    Make it easier to read accounts.urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('profile/<int:pk>', views.ProfileView.as_view(), name='profile_detail'),
     path('profile/<int:pk>/update', views.ProfileUpdateView.as_view(), name='profile_update'),
-
+    # Login and Logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    # Password Change
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-
+    # Password Reset
     path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
